1a964de444404be35a2b85cc76a24a5ae01e6a29
Nathaniel <nhanson2@nd.edu>
2020-11-13 17:18:21 -0500

    Initial project push

---
+++ project/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/nathaniel/ROS-Maze-Bot/project/src
+# Build directory: /home/nathaniel/ROS-Maze-Bot/project/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("maze_wanderer")
---
+++ project/build/maze_wanderer/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/nathaniel/ROS-Maze-Bot/project/src/maze_wanderer
+# Build directory: /home/nathaniel/ROS-Maze-Bot/project/build/maze_wanderer
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
